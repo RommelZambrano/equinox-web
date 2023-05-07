0466cb9a5361b9818b1d9228264c9b8cda95311c
Rommel Nicolás Zambrano Gaona <84996374+RommelZambrano@users.noreply.github.com>
2023-05-07 05:21:18 +0000

    add react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Helmet } from "react-helmet"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
