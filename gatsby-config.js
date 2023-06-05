116965a16de908f9470bc1b018d21beb2c974110
RommelZambrano <rnzambrano@espe.edu.ec>
2023-06-05 02:14:29 -0500

    Seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Equinox Services`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Rommel Zambrano`,
+      summary: `who lives and works in Ecuador building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Equinox Services es una empresa de servicios contables, tributarios y financieros.    `,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `RommelZambrano`,
     },
   },
   plugins: [
